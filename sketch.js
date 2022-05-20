b95660f32f0394d604958b4270884dc104795ed2
Moxil Shah <shahmv200216@gmail.com>
2022-05-19 20:29:17 -0600

    fixed getters and setters

--- sketch.js
+++ sketch.js
@@ -1,24 +1,30 @@
 let shapeArray = []; // global array of all shapes made
 let guardArray = []; // global array of all security guards made
+let allVertices = [];
+const SecurityGuardNames = ["red", "blue", "green", "yellow"];
 let pointClicked = false;
 let shapeClicked = false;
+let securityGuardClicked = false;
 let shape_i_for_shape_clicked;
 let shape_i;
 let point_j;
-let angle = 0;
+let guard_i;
 
 function setup() {
   createCanvas(720, 400);
+  frameRate(30);
 }
 
 function draw() {
   background(102);
-  renderAllShapes();
   renderAllSecurityGuards();
+  renderAllShapes();
   checkIfClickAVertex();
   checkIfClickInsideShape();
+  checkIfClickSecurityGuard();
   dragPoint();
   dragShape();
+  dragSecurityGuard();
 }
 
 // from the HTML form
@@ -29,7 +35,11 @@ function sidesInput() {
 
 // from the HTML form
 function SecurityGuardInput() {
-  guardArray.push(new SecurityGuard(400, 200, "purple"));
+  if (SecurityGuardNames.length !== 0) {
+    guardArray.push(new SecurityGuard(400, 200, SecurityGuardNames.pop()));
+  } else {
+    console.log("No more security guards available!");
+  }
 }
 
 function renderAllSecurityGuards() {
@@ -37,29 +47,13 @@ function renderAllSecurityGuards() {
     guardArray[i].drawSecurityGuard();
     push();
 
-    stroke("red");
-    strokeWeight(10);
-   
-
-    guardArray[i].updateLinePostionAfterRotate(angle, i);
+    guardArray[i].updateLinePositionAfterRotate(
+      guardArray[i].getLineAngle(),
+      i
+    );
     guardArray[i].drawLine();
 
-    if (angle <= PI / 2) {
-      angle += 0.005;
-    }
-
     pop();
-    if (angle > PI / 4) {
-      push();
-      noStroke();
-      fill(color(255, 204, 0));
-      arc(guardArray[i].getX(), guardArray[i].getY(), 100, 100, 0, PI/4);
-      arc(guardArray[i].getX(), guardArray[i].getY(), 200, 200, PI/4, angle);
-      pop();
-    }
-    else{
-      arc(guardArray[i].getX(), guardArray[i].getY(), 100, 100, 0, angle);
-    }
   }
 }
 
@@ -72,7 +66,8 @@ function polygon(x, y, radius, npoints) {
   for (let i = 0; i < TWO_PI; i += angle) {
     let sx = x + cos(i) * radius;
     let sy = y + sin(i) * radius;
-    vertexes.push([sx, sy]);
+    vertexes.push(new Point(sx, sy));
+    allVertices.push(new Point(sx, sy));
   }
 
   newShape = new Shape(x, y, npoints, vertexes);
@@ -84,7 +79,10 @@ function renderAllShapes() {
   for (let i = 0; i < shapeArray.length; i += 1) {
     beginShape();
     for (let j = 0; j < shapeArray[i].vertexArray.length; j += 1) {
-      vertex(shapeArray[i].vertexArray[j][0], shapeArray[i].vertexArray[j][1]);
+      vertex(
+        shapeArray[i].vertexArray[j].getX(),
+        shapeArray[i].vertexArray[j].getY()
+      );
     }
     endShape(CLOSE);
 
@@ -92,7 +90,10 @@ function renderAllShapes() {
     push();
     strokeWeight(10);
     for (let j = 0; j < shapeArray[i].vertexArray.length; j += 1) {
-      point(shapeArray[i].vertexArray[j][0], shapeArray[i].vertexArray[j][1]);
+      point(
+        shapeArray[i].vertexArray[j].getX(),
+        shapeArray[i].vertexArray[j].getY()
+      );
     }
     pop();
   }
@@ -103,7 +104,8 @@ function checkIfClickAVertex() {
     shapeArray.length === 0 ||
     mouseIsPressed === false ||
     pointClicked === true ||
-    shapeClicked === true
+    shapeClicked === true ||
+    securityGuardClicked === true
   ) {
     return null;
   }
@@ -113,13 +115,13 @@ function checkIfClickAVertex() {
       if (
         between(
           mouseX,
-          shapeArray[i].vertexArray[j][0] - 10,
-          shapeArray[i].vertexArray[j][0] + 10
+          shapeArray[i].vertexArray[j].getX() - 10,
+          shapeArray[i].vertexArray[j].getX() + 10
         ) &&
         between(
           mouseY,
-          shapeArray[i].vertexArray[j][1] - 10,
-          shapeArray[i].vertexArray[j][1] + 10
+          shapeArray[i].vertexArray[j].getY() - 10,
+          shapeArray[i].vertexArray[j].getY() + 10
         )
       ) {
         pointClicked = true;
@@ -135,7 +137,8 @@ function checkIfClickInsideShape() {
     shapeArray.length === 0 ||
     mouseIsPressed === false ||
     shapeClicked === true ||
-    pointClicked === true
+    pointClicked === true ||
+    securityGuardClicked === true
   ) {
     return null;
   }
@@ -164,23 +167,54 @@ function checkIfClickInsideShape() {
   }
 }
 
+function checkIfClickSecurityGuard() {
+  if (
+    guardArray.length === 0 ||
+    mouseIsPressed === false ||
+    pointClicked === true ||
+    shapeClicked === true ||
+    securityGuardClicked === true
+  ) {
+    return null;
+  }
+
+  for (let i = 0; i < guardArray.length; i += 1) {
+    if (
+      between(mouseX, guardArray[i].getX() - 10, guardArray[i].getX() + 10) &&
+      between(mouseY, guardArray[i].getY() - 10, guardArray[i].getY() + 10)
+    ) {
+      securityGuardClicked = true;
+      guard_i = i;
+    }
+  }
+}
+
 function updateVertexArrayDistancetoMousePress(i) {
   shapeArray[i].vertexArrayDistancetoMousePress = [];
   for (let j = 0; j < shapeArray[i].vertexArray.length; j += 1) {
-    deltaX = mouseX - shapeArray[i].vertexArray[j][0];
-    deltaY = mouseY - shapeArray[i].vertexArray[j][1];
+    deltaX = mouseX - shapeArray[i].vertexArray[j].getX();
+    deltaY = mouseY - shapeArray[i].vertexArray[j].getY();
     shapeArray[i].vertexArrayDistancetoMousePress.push([deltaX, deltaY]);
   }
 }
 
 function dragPoint() {
   if (pointClicked === true && mouseIsPressed === true) {
-    shapeArray[shape_i].vertexArray[point_j][0] = mouseX;
-    shapeArray[shape_i].vertexArray[point_j][1] = mouseY;
+    shapeArray[shape_i].vertexArray[point_j].setX(mouseX);
+    shapeArray[shape_i].vertexArray[point_j].setY(mouseY);
   } else if (pointClicked === true) {
     shapeArray[shape_i].setLineArray();
     updateVertexArrayDistancetoMousePress(shape_i);
     pointClicked = false;
+  }
+}
+
+function dragSecurityGuard() {
+  if (securityGuardClicked === true && mouseIsPressed === true) {
+    guardArray[guard_i].setX(mouseX);
+    guardArray[guard_i].setY(mouseY);
+  } else if (securityGuardClicked === true) {
+    securityGuardClicked = false;
   }
 }
 
@@ -192,9 +226,9 @@ function dragShape() {
       j += 1
     ) {
       deltaXCurrent =
-        mouseX - shapeArray[shape_i_for_shape_clicked].vertexArray[j][0];
+        mouseX - shapeArray[shape_i_for_shape_clicked].vertexArray[j].getX();
       deltaYCurrent =
-        mouseY - shapeArray[shape_i_for_shape_clicked].vertexArray[j][1];
+        mouseY - shapeArray[shape_i_for_shape_clicked].vertexArray[j].getY();
       deltaX =
         shapeArray[shape_i_for_shape_clicked].vertexArrayDistancetoMousePress[
           j
@@ -203,10 +237,17 @@ function dragShape() {
         shapeArray[shape_i_for_shape_clicked].vertexArrayDistancetoMousePress[
           j
         ][1];
-      shapeArray[shape_i_for_shape_clicked].vertexArray[j][0] +=
-        deltaXCurrent - deltaX;
-      shapeArray[shape_i_for_shape_clicked].vertexArray[j][1] +=
-        deltaYCurrent - deltaY;
+      shapeArray[shape_i_for_shape_clicked].vertexArray[j].setX(
+        shapeArray[shape_i_for_shape_clicked].vertexArray[j].getX() +
+          deltaXCurrent -
+          deltaX
+      );
+
+      shapeArray[shape_i_for_shape_clicked].vertexArray[j].setY(
+        shapeArray[shape_i_for_shape_clicked].vertexArray[j].getY() +
+          deltaYCurrent -
+          deltaY
+      );
     }
   } else if (shapeClicked === true) {
     shapeArray[shape_i_for_shape_clicked].setLineArray();
@@ -259,6 +300,7 @@ function checkIfIntersect(p1, q1, p2, q2) {
 
 class Shape {
   constructor(x, y, nPoints, vertexArray) {
+    this.id = Date.now();
     this.x = x;
     this.y = y;
     this.nPoints = nPoints;
@@ -271,19 +313,19 @@ class Shape {
     this.lineArray = [];
     for (let i = 0; i < this.vertexArray.length - 1; i++) {
       let aLine = new Line(
-        this.vertexArray[i][0],
-        this.vertexArray[i][1],
-        this.vertexArray[i + 1][0],
-        this.vertexArray[i + 1][1]
+        this.vertexArray[i].getX(),
+        this.vertexArray[i].getY(),
+        this.vertexArray[i + 1].getX(),
+        this.vertexArray[i + 1].getY()
       );
       this.lineArray.push(aLine);
     }
 
     let aLine = new Line(
-      this.vertexArray[this.vertexArray.length - 1][0],
-      this.vertexArray[this.vertexArray.length - 1][1],
-      this.vertexArray[0][0],
-      this.vertexArray[0][1]
+      this.vertexArray[this.vertexArray.length - 1].getX(),
+      this.vertexArray[this.vertexArray.length - 1].getY(),
+      this.vertexArray[0].getX(),
+      this.vertexArray[0].getY()
     );
     this.lineArray.push(aLine);
   }
@@ -295,7 +337,6 @@ class Line {
     this.y_1 = y_1;
     this.x_2 = x_2;
     this.y_2 = y_2;
-    this.test = 0;
     this.Point1 = new Point(this.x_1, this.y_1);
     this.Point2 = new Point(this.x_2, this.y_2);
     this.length = Math.sqrt(
@@ -316,7 +357,7 @@ class Line {
     line(this.x_1, this.y_1, this.x_2, this.y_2);
   }
 
-  updateLinePostionAfterRotate(angle, i) {
+  updateLinePositionAfterRotate(angle, i) {
     this.x_1 = guardArray[i].getX();
     this.y_1 = guardArray[i].getY();
     this.x_2 = cos(angle) * this.length + guardArray[i].getX();
@@ -329,21 +370,39 @@ class Point {
     this.x = x;
     this.y = y;
   }
+
+  setX(x) {
+    this.x = x;
+  }
+
+  setY(y) {
+    this.y = y;
+  }
+
+  getX() {
+    return this.x;
+  }
+
+  getY() {
+    return this.y;
+  }
 }
 
 class SecurityGuard {
-  constructor(x, y, color) {
+  constructor(x, y, name) {
     this.x = x;
     this.y = y;
-    this.color = color;
+    this.name = name;
     this.size = 13;
     this.line = new Line(0, 0, 100, 0);
+    this.lineAngle = 0;
+    this.sortedVertices = [];
   }
 
   drawSecurityGuard() {
     push();
     strokeWeight(this.size);
-    stroke(this.color);
+    stroke(this.name);
     point(this.x, this.y);
     pop();
   }
@@ -352,8 +411,20 @@ class SecurityGuard {
     this.line.drawLine();
   }
 
-  updateLinePostionAfterRotate(angle, i) {
-    this.line.updateLinePostionAfterRotate(angle, i);
+  updateLinePositionAfterRotate(angle, i) {
+    this.line.updateLinePositionAfterRotate(angle, i);
+  }
+
+  setX(x) {
+    this.x = x;
+  }
+
+  setY(y) {
+    this.y = y;
+  }
+
+  setLineAngle(angle) {
+    this.lineAngle = angle;
   }
 
   getX() {
@@ -369,10 +440,14 @@ class SecurityGuard {
   }
 
   getColor() {
-    return this.color;
+    return this.name;
   }
 
   getLine() {
     return this.line;
+  }
+
+  getLineAngle() {
+    return this.lineAngle;
   }
 }
