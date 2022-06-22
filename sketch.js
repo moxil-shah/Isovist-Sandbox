547a8670fa15b11f1f309c3d925b68ae9a86e3b2
Moxil Shah <shahmv200216@gmail.com>
2022-06-21 18:53:01 -0600

    file reorg

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-
-
 let allShapes = new Set(); // global array of all shapes made
 let allguards = new Set(); // global array of all security guards made
 const SecurityGuardNames = [
@@ -12,7 +10,7 @@ let shapeClicked = false;
 let securityGuardClicked = false;
 let doubleClick = false;
 
-let HEXAGON_ROUNDING_ERROR = 1e-15;
+const HEXAGON_ROUNDING_ERROR = 1e-15;
 let shapeDragged = -1;
 let shapesPointDragged;
 let pointDragged = -1;
@@ -379,120 +377,6 @@ function dragShape() {
       guard.sortVertices();
     }
   }
-}
-
-function findIntersection(line1, line2) {
-  let px =
-    ((line1.getPoint1().getX() * line1.getPoint2().getY() -
-      line1.getPoint1().getY() * line1.getPoint2().getX()) *
-      (line2.getPoint1().getX() - line2.getPoint2().getX()) -
-      (line1.getPoint1().getX() - line1.getPoint2().getX()) *
-        (line2.getPoint1().getX() * line2.getPoint2().getY() -
-          line2.getPoint1().getY() * line2.getPoint2().getX())) /
-    ((line1.getPoint1().getX() - line1.getPoint2().getX()) *
-      (line2.getPoint1().getY() - line2.getPoint2().getY()) -
-      (line1.getPoint1().getY() - line1.getPoint2().getY()) *
-        (line2.getPoint1().getX() - line2.getPoint2().getX()));
-  let py =
-    ((line1.getPoint1().getX() * line1.getPoint2().getY() -
-      line1.getPoint1().getY() * line1.getPoint2().getX()) *
-      (line2.getPoint1().getY() - line2.getPoint2().getY()) -
-      (line1.getPoint1().getY() - line1.getPoint2().getY()) *
-        (line2.getPoint1().getX() * line2.getPoint2().getY() -
-          line2.getPoint1().getY() * line2.getPoint2().getX())) /
-    ((line1.getPoint1().getX() - line1.getPoint2().getX()) *
-      (line2.getPoint1().getY() - line2.getPoint2().getY()) -
-      (line1.getPoint1().getY() - line1.getPoint2().getY()) *
-        (line2.getPoint1().getX() - line2.getPoint2().getX()));
-  return new Point(px, py, null);
-}
-
-function between(theThing, min, max) {
-  return theThing >= min && theThing <= max;
-}
-
-function onSegment(p, q, r) {
-  if (
-    q.x <= Math.max(p.x, r.x) &&
-    q.x >= Math.min(p.x, r.x) &&
-    q.y <= Math.max(p.y, r.y) &&
-    q.y >= Math.min(p.y, r.y)
-  )
-    return true;
-
-  return false;
-}
-
-function orientOrder(p, q, r) {
-  let val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-
-  if (val == 0) return 0;
-
-  return val > 0 ? 1 : 2;
-}
-
-function distanceBetweenTwoPoints(p1, p2) {
-  return Math.sqrt((p1.getX() - p2.getX()) ** 2 + (p1.getY() - p2.getY()) ** 2);
-}
-
-function checkIfIntersect(line1, line2) {
-  let p1 = line1.getPoint1();
-  let q1 = line1.getPoint2();
-  let p2 = line2.getPoint1();
-  let q2 = line2.getPoint2();
-
-  let o1 = orientOrder(p1, q1, p2);
-  let o2 = orientOrder(p1, q1, q2);
-  let o3 = orientOrder(p2, q2, p1);
-  let o4 = orientOrder(p2, q2, q1);
-
-  if (o1 != o2 && o3 != o4) return true;
-
-  if (o1 == 0 && onSegment(p1, p2, q1)) return true;
-
-  if (o2 == 0 && onSegment(p1, q2, q1)) return true;
-
-  if (o3 == 0 && onSegment(p2, p1, q2)) return true;
-
-  if (o4 == 0 && onSegment(p2, q1, q2)) return true;
-
-  return false;
-}
-
-function checkIfTwoPointsOverlap(p1, p2) {
-  return p1.getX() === p2.getX() && p1.getY() === p2.getY();
-}
-
-function checkIfTwoPointsOverlapRounded(p1, p2) {
-  return (
-    Math.round(p1.getX() * 100) / 100 === Math.round(p2.getX() * 100) / 100 &&
-    Math.round(p1.getY() * 100) / 100 === Math.round(p2.getY() * 100) / 100
-  );
-}
-
-function checkIfVertexIsEndPointOfALine(aVertex, aLine) {
-  return (
-    checkIfTwoPointsOverlap(aVertex, aLine.getPoint1()) ||
-    checkIfTwoPointsOverlap(aVertex, aLine.getPoint2())
-  );
-}
-
-function checkIfTwoLinesIntersectOnEndPoints(line1, line2) {
-  return (
-    checkIfTwoPointsOverlap(line1.getPoint1(), line2.getPoint1()) ||
-    checkIfTwoPointsOverlap(line1.getPoint2(), line2.getPoint2()) ||
-    checkIfTwoPointsOverlap(line1.getPoint1(), line2.getPoint2()) ||
-    checkIfTwoPointsOverlap(line1.getPoint2(), line2.getPoint1())
-  );
-}
-
-function checkIfTwoLinesIntersectOnEndPointsRounded(line1, line2) {
-  return (
-    checkIfTwoPointsOverlapRounded(line1.getPoint1(), line2.getPoint1()) ||
-    checkIfTwoPointsOverlapRounded(line1.getPoint2(), line2.getPoint2()) ||
-    checkIfTwoPointsOverlapRounded(line1.getPoint1(), line2.getPoint2()) ||
-    checkIfTwoPointsOverlapRounded(line1.getPoint2(), line2.getPoint1())
-  );
 }
 
 class Shape {
